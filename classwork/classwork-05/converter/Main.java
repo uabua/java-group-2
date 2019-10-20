0dc0856e78a61fe4402a4df955bf2c635dbfe725
uabua <mariami.chakhvadze.1@btu.edu.ge>
2019-10-20 21:31:28 +0400

    Easy converter

--- classwork/classwork-05/converter/Main.java
+++ classwork/classwork-05/converter/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         EasyConverter fromDecimal = new EasyConverter();
+
         fromDecimal.exerciseTen(4);
         fromDecimal.exerciseEleven(10);
     }
