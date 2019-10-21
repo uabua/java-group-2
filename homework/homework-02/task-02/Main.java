78214e3b7835896881102f29f8ef1816c12967f7
uabua <mariami.chakhvadze.1@btu.edu.ge>
2019-10-22 02:03:38 +0400

    Updated task 2

--- homework/homework-02/task-02/Main.java
+++ homework/homework-02/task-02/Main.java
@@ -1,7 +1,6 @@
 ﻿package com.company.task2;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,20 +17,19 @@ public class Main {
     მნიშვნელობებს შორის მინიმალურს და მაქსიმალურს.
      */
     private static void exerciseTwo() {
-        List<Double> values = new ArrayList<>();
+        List<Double> values = new ArrayList<>(); // წაკითხვის შემდეგ ფუნქციის მნიშვნელობების შესანახად
         String line;
 
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader("function.txt"));
 
+            // ფაილიდან მხოლოდ ფუნქციის მნიშვნელობის (საბოლოო double-ის) ამოღება
             while ((line = bufferedReader.readLine()) != null) {
                 values.add(Double.parseDouble(line.substring(line.lastIndexOf(" ") + 1)));
             }
 
             System.out.println(String.format("მინიმალური მნიშვნელობაა: %.2f", Collections.min(values)));
             System.out.println(String.format("მაქსიმალური მნიშვნელობაა: %.2f", Collections.max(values)));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
