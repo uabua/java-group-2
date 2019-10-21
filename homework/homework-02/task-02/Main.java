a9b8118614da555ef39ffe377bd195fb7b208d0b
uabua <mariami.chakhvadze.1@btu.edu.ge>
2019-10-22 01:54:33 +0400

    task 2

---
+++ homework/homework-02/task-02/Main.java
@@ -0,0 +1,39 @@
+﻿package com.company.task2;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        exerciseTwo();
+    }
+
+    /*
+    დაწერეთ პროგრამა, რომელიც function.txt ფაილიდან (იხილეთ ამოცანა 1)წაიკითხავს მონაცემებს და დაბეჭდავს
+    მნიშვნელობებს შორის მინიმალურს და მაქსიმალურს.
+     */
+    private static void exerciseTwo() {
+        List<Double> values = new ArrayList<>();
+        String line;
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("function.txt"));
+
+            while ((line = bufferedReader.readLine()) != null) {
+                values.add(Double.parseDouble(line.substring(line.lastIndexOf(" ") + 1)));
+            }
+
+            System.out.println(String.format("მინიმალური მნიშვნელობაა: %.2f", Collections.min(values)));
+            System.out.println(String.format("მაქსიმალური მნიშვნელობაა: %.2f", Collections.max(values)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
